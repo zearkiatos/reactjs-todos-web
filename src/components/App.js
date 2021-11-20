330ca3dda7d59526c7f19dc4179c39606bb67207
Pedro Capriles <caprilespe@outlook.com>
2021-11-20 12:08:50 -0300

    Analisys the composition

--- src/components/App.js
+++ src/components/App.js
@@ -1,64 +1,15 @@
 import React, { Fragment, useState } from 'react';
-// import { TodoProvider } from '../TodoContext';
-// import TodoList from './TodoList';
-// import TodoHeader from './TodoHeader';
-// import UI from './UI';
-
+import { TodoProvider } from '../TodoContext';
+import TodoList from './TodoList';
+import TodoHeader from './TodoHeader';
+import UI from './UI';
 
 const App = () => {
-  const [state, setState] = useState('shared state');
   return (
-    <Fragment>
-      <TodoHeader>
-        <TodoCounter />
-        <TodoSearch />
-      </TodoHeader>
-      <TodoList>
-        <TodoItem state={state} />
-      </TodoList>
-    </Fragment>
+    <TodoProvider>
+      <UI/>
+    </TodoProvider>
   );
 };
-
-const TodoHeader = ({ children }) => {
-  return (
-    <header>
-      {children}
-    </header>
-  )
-};
-
-const TodoList = ({ children }) => {
-  return (
-    <section className="todolist-container">
-      {children}
-    </section>
-  )
-};
-
-const TodoCounter = () => {
-  return <p>TodoCounter</p>
-};
-
-const TodoSearch = () => {
-  return <p>TodoSearch</p>
-};
-
-const TodoItem = ({ state }) => {
-  return <p>TodoItem: {state}</p>
-};
-
-
-// const App = () => {
-//   return ;
-// }
-
-// const App = () => {
-//   return (
-//     <TodoProvider>
-//       <UI/>
-//     </TodoProvider>
-//   );
-// };
 
 export default App;
