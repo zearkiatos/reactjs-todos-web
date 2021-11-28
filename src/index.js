fa31db8871a4be5a88c23cd67bd934c66ac36ccd
zearkiatos <caprilespe@outlook.com>
2021-11-28 12:16:11 -0300

    Creating your first High Order Component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/App";
+// import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const App = (props) => {
+    return (
+        <h1>{props.greeting}, {props.name}</h1>
+    );
+};
+
+
+const withGreeting = (WrappedComponent) => {
+    return function WrappedComponentWithGreeting(greeting) {
+        return function component(props) {
+            return (
+                <Fragment>
+                    <WrappedComponent {...props} greeting={greeting} />
+                    <p>Estamos Acompañando al WrappedComponent</p>
+                </Fragment>
+            )
+        }
+    }
+}
+
+const AppWithGreeting = withGreeting (App)('Buenas!');
+
+ReactDOM.render(<AppWithGreeting name='Pedro' />, document.getElementById("root"));
